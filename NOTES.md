219060ae4dc6822837ea3b4491f0b4dd5b41c708
Jack <ajackofsometrades@gmail.com>
2019-02-07 22:10:11 -0800

    example of css in js using material ui

---
+++ NOTES.md
@@ -0,0 +1,1 @@
+Learn more about service workers: http://bit.ly/CRA-PWA
--- src/views/content/Cards/index.js
+++ src/views/content/Cards/index.js
@@ -1,48 +1,43 @@
-import React,  { Component } from 'react';
+import React from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Grid from '@material-ui/core/Grid';
 
 import CardItem from './CardItem';
 import SectionHeader from '../../lib/text/SectionHeader';
+import { styled } from '@material-ui/styles';
+
 const backgroundShape = require('../../../assets/images/shape.svg');
 
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.grey['A500'],
-    overflow: 'hidden',
-    background: `url(${backgroundShape}) no-repeat`,
-    backgroundSize: 'cover',
-    backgroundPosition: '0 400px',
-    marginTop: 20,
-    padding: 20,
-    paddingBottom: 200
-  },
-  grid: {
-    width: 1000
-  }
-})
+const RootDiv = styled('div')({
+  flexGrow: 1,
+  // backgroundColor: theme.palette.grey['A500'],
+  overflow: 'hidden',
+  background: `url(${backgroundShape}) no-repeat`,
+  backgroundSize: 'cover',
+  backgroundPosition: '0 400px',
+  marginTop: 20,
+  padding: 20,
+  paddingBottom: 200
+});
 
-class Cards extends Component {
+const StyledGrid = styled(Grid)({
+  width: 1000
+});
 
-  render() {
-    const { classes } = this.props;
+const Cards = ({ classes }) => {
 
-    return (
-      <React.Fragment>
-        <div className={classes.root}>
-          <Grid container justify="center">
-            <Grid spacing={24} alignItems="center" justify="center" container className={classes.grid}>
-              <Grid item xs={12}>
-                <SectionHeader title="Cards" subtitle="One page with a list of a collection" />
-                <CardItem />
-              </Grid>
-            </Grid>
+  return (
+    <RootDiv>
+      <Grid container justify="center">
+        <StyledGrid spacing={24} alignItems="center" justify="center" container>
+          <Grid item xs={12}>
+            <SectionHeader title="Cards" subtitle="One page with a list of a collection" />
+            <CardItem />
           </Grid>
-        </div>
-      </React.Fragment>
-    )
-  }
-}
+        </StyledGrid>
+      </Grid>
+    </RootDiv>
+  );
+};
 
-export default withStyles(styles)(Cards);
+export default Cards;
