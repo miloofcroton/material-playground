e82fa023f9bd7cff745b366ff212c9079324e180
Jack <ajackofsometrades@gmail.com>
2019-02-08 01:42:04 -0800

    converted entire card item to css in js

--- src/views/content/Cards/CardItem.js
+++ src/views/content/Cards/CardItem.js
@@ -1,144 +1,134 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { styled } from '@material-ui/styles';
+import muiTheme from '../../layout/theme/mui';
 
-import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
 import DescriptionIcon from '@material-ui/icons/Description';
 import ButtonBar from '../../lib/buttons/ButtonBar';
 
-const RootDiv = styled('div')({
-  flexGrow: 1,
-  // backgroundColor: theme.palette.grey['A500'],
-  overflow: 'hidden',
-  // background: `url(${backgroundShape}) no-repeat`,
-  backgroundSize: 'cover',
-  backgroundPosition: '0 400px',
-  marginTop: 20,
-  padding: 20,
-  paddingBottom: 200
+const PaperStyled = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing.unit * 3,
+  textAlign: 'left',
+  color: theme.palette.text.secondary
+}), { defaultTheme: muiTheme });
+
+const AvatarStyled = styled(Avatar)(({ theme }) => ({
+  margin: 10,
+  backgroundColor: theme.palette.grey['200'],
+  color: theme.palette.text.primary,
+}), { defaultTheme: muiTheme });
+
+const AvatarContainer = styled('div')(({ theme }) => ({
+  [theme.breakpoints.down('sm')]: {
+    marginLeft: 0,
+    marginBottom: theme.spacing.unit * 4
+  }
+}), { defaultTheme: muiTheme });
+
+const ItemContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+  [theme.breakpoints.down('sm')]: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center'
+  }
+}), { defaultTheme: muiTheme });
+
+const Baseline = styled('div')(({ theme }) => ({
+  alignSelf: 'baseline',
+  marginLeft: theme.spacing.unit * 4,
+  [theme.breakpoints.down('sm')]: {
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+    alignItems: 'center',
+    width: '100%',
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2,
+    marginLeft: 0
+  }
+}), { defaultTheme: muiTheme });
+
+const Inline = styled('div')(({ theme }) => ({
+  display: 'inline-block',
+  marginLeft: theme.spacing.unit * 4,
+  [theme.breakpoints.down('sm')]: {
+    marginLeft: 0
+  }
+}), { defaultTheme: muiTheme });
+
+const InlineRight = styled('div')(({ theme }) => ({
+  width: '30%',
+  textAlign: 'right',
+  marginLeft: 50,
+  alignSelf: 'flex-end',
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    margin: 0,
+    textAlign: 'center'
+  }
+}), { defaultTheme: muiTheme });
+
+const UpperType = styled(Typography)({
+  textTransform: 'uppercase'
 });
 
+const CardItem = () => {
 
-const styles = theme => ({
-  paper: {
-    padding: theme.spacing.unit * 3,
-    textAlign: 'left',
-    color: theme.palette.text.secondary
-  },
-  avatar: {
-    margin: 10,
-    backgroundColor: theme.palette.grey['200'],
-    color: theme.palette.text.primary,
-  },
-  avatarContainer: {
-    [theme.breakpoints.down('sm')]: {
-      marginLeft: 0,
-      marginBottom: theme.spacing.unit * 4
-    }
-  },
-  itemContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    [theme.breakpoints.down('sm')]: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center'
-    }
-  },
-  baseline: {
-    alignSelf: 'baseline',
-    marginLeft: theme.spacing.unit * 4,
-    [theme.breakpoints.down('sm')]: {
-      display: 'flex',
-      flexDirection: 'column',
-      textAlign: 'center',
-      alignItems: 'center',
-      width: '100%',
-      marginTop: theme.spacing.unit * 2,
-      marginBottom: theme.spacing.unit * 2,
-      marginLeft: 0
-    }
-  },
-  inline: {
-    display: 'inline-block',
-    marginLeft: theme.spacing.unit * 4,
-    [theme.breakpoints.down('sm')]: {
-      marginLeft: 0
-    }
-  },
-  inlineRight: {
-    width: '30%',
-    textAlign: 'right',
-    marginLeft: 50,
-    alignSelf: 'flex-end',
-    [theme.breakpoints.down('sm')]: {
-      width: '100%',
-      margin: 0,
-      textAlign: 'center'
-    }
-  },
-  backButton: {
-    marginRight: theme.spacing.unit * 2
-  }
-})
+  return (
+    <PaperStyled>
+      <ItemContainer>
 
-class CardItem extends Component {
+        <AvatarContainer>
+          <AvatarStyled>
+            <DescriptionIcon/>
+          </AvatarStyled>
+        </AvatarContainer>
 
-  render() {
-    const { classes } = this.props;
+        <Baseline>
+          <Inline>
+            <UpperType color='secondary' gutterBottom>
+              Months
+            </UpperType>
+            <Typography variant="h6" gutterBottom>
+              4 month(s)
+            </Typography>
+          </Inline>
+          <Inline>
+            <UpperType color='secondary' gutterBottom>
+              Creation date
+            </UpperType>
+            <Typography variant="h6" gutterBottom>
+              01 February 2019
+            </Typography>
+          </Inline>
+          <Inline>
+            <UpperType color='secondary' gutterBottom>
+              Amount
+            </UpperType>
+            <Typography variant="h6" gutterBottom>
+              6,600 USD
+            </Typography>
+          </Inline>
+        </Baseline>
 
-    return (
-      <RootDiv>
-        <Paper className={classes.paper}>
-          <div className={classes.itemContainer}>
-            <div className={classes.avatarContainer}>
-              <Avatar className={classes.avatar}>
-                <DescriptionIcon />
-              </Avatar>
-            </div>
-            <div className={classes.baseline}>
-              <div className={classes.inline}>
-                <Typography style={{ textTransform: 'uppercase' }} color='secondary' gutterBottom>
-                  Months
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                  4 month(s)
-                </Typography>
-              </div>
-              <div className={classes.inline}>
-                <Typography style={{ textTransform: 'uppercase' }} color='secondary' gutterBottom>
-                  Creation date
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                  01 February 2019
-                </Typography>
-              </div>
-              <div className={classes.inline}>
-                <Typography style={{ textTransform: 'uppercase' }} color='secondary' gutterBottom>
-                  Amount
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                  6,600 USD
-                </Typography>
-              </div>
-            </div>
-            <div className={classes.inlineRight}>
-              <Typography style={{ textTransform: 'uppercase' }} color='secondary' gutterBottom>
-                Other Amount
-              </Typography>
-              <Typography variant="h4" gutterBottom>
-                Once a month
-              </Typography>
-              <ButtonBar />
-            </div>
-          </div>
-        </Paper>
-      </RootDiv>
-    )
-  }
-}
+        <InlineRight>
+          <UpperType color='secondary' gutterBottom>
+            Other Amount
+          </UpperType>
+          <Typography variant="h4" gutterBottom>
+            Once a month
+          </Typography>
+          <ButtonBar />
+        </InlineRight>
 
-export default withStyles(styles)(CardItem);
+      </ItemContainer>
+    </PaperStyled>
+  )
+};
+
+export default CardItem;
